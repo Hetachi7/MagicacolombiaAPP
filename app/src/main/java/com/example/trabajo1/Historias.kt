766e4115e05640f77e375c2c19759695354d9a88
axelcastr <114270665+axelcastr@users.noreply.github.com>
2023-10-03 17:37:29 -0500

    commitgoddofuck

--- app/src/main/java/com/example/trabajo1/Historias.kt
+++ app/src/main/java/com/example/trabajo1/Historias.kt
@@ -1,8 +1,10 @@
 package com.example.trabajo1
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 class Historias : AppCompatActivity() {
@@ -17,11 +19,16 @@ class Historias : AppCompatActivity() {
             val intent = Intent(this@Historias, Historia1fragment::class.java)
             startActivity(intent)
         }
-        val Historia2card = findViewById<CardView>(R.id.Historia2card)
+       /* val Historia2card = findViewById<CardView>(R.id.Historia2.
+
+
+
+
+        card)
         Historia2card.setOnClickListener {
             val intent2 = Intent(this@Historias, Historia2fragment::class.java)
             startActivity(intent2)
-        }
+        }*/
         val Historia3card = findViewById<CardView>(R.id.Historia3card)
         Historia3card.setOnClickListener {
             val intent3 = Intent(this@Historias, Historia3fragment::class.java)
@@ -37,10 +44,17 @@ class Historias : AppCompatActivity() {
             val intent5 = Intent(this@Historias, Historia5fragment::class.java)
             startActivity(intent5)
         }
-        val Historia6card = findViewById<CardView>(R.id.Historia6card)
+        /*val Historia6card = findViewById<CardView>(R.id.Historia6card)
         Historia6card.setOnClickListener {
             val intent6 = Intent(this@Historias, Historia6fragment::class.java)
             startActivity(intent6)
-        }
+        }*/
+
+        val Historia6Card = findViewById<CardView>(R.id.Historia6card)
+        Historia6Card.setOnClickListener( object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                supportFragmentManager.beginTransaction().replace(R.id.Historia6card,Historia6fragment()).commit()
+            }
+        })
     }
 }
